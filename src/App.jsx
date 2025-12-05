0b40cc131dd99c9b388cdddb6bb6d050724f50a4
Jiban Chaudhary <rajbanshijiban288@gmail.com>
2025-12-05 11:08:12 +0545

    Initial commit: fixed Tailwind + Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import './index.css';
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-4xl text-red-500 font-bold">Home</h1>
+    </>
+  )
+}
+
+export default App
